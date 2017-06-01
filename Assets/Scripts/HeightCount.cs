cbf8d5080b14c4543505249a6eec553d971aaa19
fasantos83 <fasantos83@gmail.com>
2017-06-01 02:04:39 -0300

    init

---
+++ Assets/Scripts/HeightCount.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeightCount : MonoBehaviour {
+
+	public Transform player;
+	int maxHeight;
+	Text text;
+	// Use this for initialization
+	void Start () {
+		maxHeight = 0;
+		text = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int height = (int) player.position.y;
+		if (height > maxHeight) {
+			maxHeight = height;
+			text.text = height.ToString();
+		}
+	}
+}
